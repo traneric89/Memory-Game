155a2632c4c0e21a4ef44c10aa9a612e2c1816c0
optiX <55358375+traneric89@users.noreply.github.com>
2022-03-26 23:29:43 -0400

    Added css media queries

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,6 +4,7 @@ const Card = ({ teamNo, onClick }) => {
   return (
     <div>
       <img
+        className="images"
         src={require("../images/" + teamNo + ".png")}
         alt=""
         onClick={() => onClick(teamNo)}
